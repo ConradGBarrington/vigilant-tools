26fcb26f76a419cae97b49a9a193be4eaf4c4844
Conrad Barrington <conradgbarrington@gmail.com>
2023-11-05 19:12:39 -0500

    removed a leftover comment from the tutorial project

--- src/main/java/com/vigilant/tools/VigilantToolsMod.java
+++ src/main/java/com/vigilant/tools/VigilantToolsMod.java
@@ -91,10 +91,6 @@ public class VigilantToolsMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
 		LOGGER.info("Hello Fabric world!");
 
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(content -> {
